0101a18947ae5f4520de9296f3902e213c4ccf77
DenAst17 <denis2004ast@gmail.com>
2022-03-18 23:56:23 +0200

    Calculator working

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -2,19 +2,58 @@
 export default {
   data() {
     return {
-      num1: ''
+      NumPlus1: "",
+      NumPlus2: "",
+      NumMinus1: "",
+      NumMinus2: "",
+      NumMult1: "",
+      NumMult2: "",
+      NumDiv1: "",
+      NumDiv2: "",
+      NumPow1: "",
+      NumPow2: ""
     }
   }
-}
+};
 </script>
 
 <template>
-  <div class = "calc"> 
-    <input v-model = "num1">
-    <div> + </div>
-    <input v-model = "num2">
-    <div> = </div>
-    <input v-model = "num3"> 
+  <div class = "calculator">
+    <div class="calc">
+      <input size="number" v-model="NumPlus1" placeholder="Input a number" />
+      <p>+</p>
+      <input size="number" v-model="NumPlus2" placeholder="Input a number" />
+      <div>=</div>
+      <p>{{ Number(NumPlus1) + Number(NumPlus2) }}</p>
+    </div>
+    <div class="calc">
+      <input v-model="NumMinus1" placeholder="Input a number" />
+      <p>-</p>
+      <input v-model="NumMinus2" placeholder="Input a number" />
+      <div>=</div>
+      <p>{{ Number(NumMinus1) - Number(NumMinus2) }}</p>
+    </div>
+    <div class="calc">
+      <input v-model="NumMult1" placeholder="Input a number" />
+      <p>*</p>
+      <input v-model="NumMult2" placeholder="Input a number" />
+      <div>=</div>
+      <p>{{ Number(NumMult1) * Number(NumMult2) }}</p>
+    </div>
+    <div class="calc">
+      <input v-model="NumDiv1" placeholder="Input a number" />
+      <p>/</p>
+      <input v-model="NumDiv2" placeholder="Input a number" />
+      <div>=</div>
+      <p>{{ Number(NumDiv1) / Number(NumDiv2) }}</p>
+    </div>
+    <div class="calc">
+      <input v-model="NumPow1" placeholder="Input a number" />
+      <p>^</p>
+      <input v-model="NumPow2" placeholder="Input a number" />
+      <div>=</div>
+      <p>{{ Math.pow(Number(NumPow1), Number(NumPow2)) }}</p>
+    </div>
   </div>
 </template>
 
@@ -25,7 +64,12 @@ export default {
     display: flex;
   }
 }
-.calc{
+.calc {
   display: flex;
+  width: auto;
+}
+.calculator{
+  margin: 0px auto auto 30%;
+  width: auto;
 }
 </style>
