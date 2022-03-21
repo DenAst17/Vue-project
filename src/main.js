fd474cebbd4f46aca52dda6fff8df1847bc97cf6
DenAst17 <denis2004ast@gmail.com>
2022-03-21 22:59:29 +0200

    +axios and problem in com
    There is a problem.
    In main.js there is a commented code that does not work, the code should did a request, but the request module can't normally import

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+//import * as request from "request";
 
 const app = createApp(App);
 
 app.use(router);
 
 app.mount("#app");
+
+/*request("https://swapi.dev/api/people/1", { json: true }, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(body.url);
+  console.log(body.explanation);
+});
+*/
--- src/request.js
+++ src/request.js
@@ -1,1 +1,2 @@
 console.log(123);
+// Please go watch to main.js, because I tested the problem in that file
