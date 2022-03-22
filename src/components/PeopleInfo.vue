2db543bb22a67dad3e0ff268b1e5dd4df1651eed
DenAst17 <denis2004ast@gmail.com>
2022-03-22 13:26:41 +0200

    getInfo success

--- src/components/PeopleInfo.vue
+++ src/components/PeopleInfo.vue
@@ -10,11 +10,25 @@ export default {
   },
   methods: {
     addPerson() {
-      this.people.push({ id: id++, text: request.name + " " + request.height });
+      
+      request.getInfo("planets").then(function(value)
+        {
+          //console.table(value);
+          self.people = self.people.filter((t) => t == 'true');
+          for(let i = 0; i < value.length; i++)
+          {
+            let txt = "Name: " + value[i][0] + " height: " + value[i][1];
+            console.log(txt);
+            self.people.push({ id: id++, text: txt});
+          }
+        });
+      var self = this;
+      this.people = people;
+      this.people.push({ id: id++, text: 1});
       this.newPerson = "";
     },
     removePerson(person) {
-      this.people = this.people.filter((t) => t !== person);
+      
     },
   },
 };
@@ -27,7 +41,6 @@ export default {
   <ol>
     <li v-for="person in people" :key="person.id">
       {{ person.text }}
-      <button @click="removePerson(person)">X</button>
     </li>
   </ol>
 </template>
--- src/request.js
+++ src/request.js
@@ -1,21 +1,27 @@
 import axios from 'axios';
-let people_ar = [];
-let promise = new Promise(function(resolve, reject) {
-  setTimeout(() => resolve("done!"), 1000);
-});
-
-for(let i = 1; i < 10; i++)
-{
-  axios.get('https://swapi.dev/api/people/' + i)
-  .then(response => {
-    name = response.data.name;
-    height = response.data.height;
-    people_ar.push({name, height});
-  })
-  .catch(error => {
-    height = -1;
+let name = '';
+let height = -1;
+export function getInfo(str){
+  let people_ar = []; 
+  return new Promise((resolve, reject) => {
+    for(let i = 1; i <= 100; i++)
+    {
+      axios.get('https://swapi.dev/api/' + str + '/' + i)
+      .then(response => {
+        name = response.data.name;
+        if(str == 'people')
+          height = response.data.height;
+        else if(str == 'planets')
+          height = response.data.diameter;
+        else
+          height = response.data.length;
+        people_ar.push([name, height]);
+      })
+      .catch(error => {
+        height = -1;
+      });
+    }
+    setTimeout(() => resolve(people_ar), 5000);
   });
 }
-export{people_ar};
-console.log(people_ar);
-console.log(123);
+console.log(123); 
