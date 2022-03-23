2eeaf001fde9366a38ad451bc78d9ebd36f00d1b
DenAst17 <denis2004ast@gmail.com>
2022-03-23 23:41:02 +0200

    Live list generation and design implementation

--- src/components/PeopleInfo.vue
+++ src/components/PeopleInfo.vue
@@ -5,18 +5,35 @@ export default {
   data() {
     return {
       requestInfo: "Please, press the button to get request",
+      sortedrequestInfo: "",
       people: [],
+      sortedpeople: []      
     };
   },
   methods: {
     addPerson() {
-      this.requestInfo = "Wait for 5 seconds to get the list";
+      var self = this;
+      this.requestInfo = "Wait for list to download";
       async function f1()
         {
-          value = await request.getInfo("people");
-          self.requestInfo = "The list of people (sorted by height):"
-          //console.table(value);
           self.people = self.people.filter((t) => t == 'true');
+          self.sortedpeople = self.sortedpeople.filter((t) => t == 'true');
+          let value = [];
+          for(let i = 0; i < 100; i++)
+          {
+            let response = await request.getInfo("people", i + 1)
+            if(response == undefined)
+              continue;
+            //console.log(typeof(response));
+            let name = response.data.name;
+            let height = response.data.height;
+            let txt = "Name: " + name + ", height: " + height + " см";
+            value.push([name, height]);
+            self.people.push({ id: id++, text: txt});
+            //console.log([name, height]);
+          }
+          //console.table(value);
+          self.requestInfo = "The list of people:"
           value.sort(function(a, b) {
             return a[1] - b[1];
           });
@@ -24,11 +41,11 @@ export default {
           {
             let txt = "Name: " + value[i][0] + ", height: " + value[i][1] + " см";
             console.log(txt);
-            self.people.push({ id: id++, text: txt});
+            self.sortedpeople.push({ id: id++, text: txt});
           }
+          self.sortedrequestInfo = "The list of people sorted by height";
         };
       f1();
-      var self = this;
       this.people = people;
       this.people.push({ id: id++, text: 1});
     }
@@ -37,19 +54,47 @@ export default {
 </script>
 
 <template>
-  <form @submit.prevent="addPerson">
+  <form @submit.prevent="addPerson" class = "btndiv">
     <button class = "getinfo">Get info</button>
-    <p class = "getinfo">{{requestInfo}}</p>
   </form>
-  <ol>
-    <li v-for="person in people" :key="person.id">
-      {{ person.text }}
-    </li>
-  </ol>
+  <div class = 'lists'>
+    <div>
+      <p class = "getinfo">{{requestInfo}}</p>
+      <ol>
+        <li v-for="person in people" :key="person.id">
+          {{ person.text }}
+        </li>
+      </ol>
+    </div>
+    <div>
+      <p class = "getinfo">{{sortedrequestInfo}}</p>
+      <ol>
+        <li v-for="person in sortedpeople" :key="person.id">
+          {{ person.text }}
+        </li>
+      </ol>
+    </div>
+  </div>
+  
 </template>
 
 <style>
+.btndiv{
+  display:flex;
+  width: 86.05px;
+  margin-left: auto;
+  margin-right: auto;
+  align-content: center;
+  align-items: center;
+}
 .getinfo{
   font-size:20px;
+}
+.lists{
+  display:flex;
+  width: 639.16px;
+  margin-left: auto;
+  margin-right: auto;
+  display:flex;
 }
 </style>
--- src/request.js
+++ src/request.js
@@ -1,28 +1,15 @@
 import axios from 'axios';
 let name = '';
 let height = -1;
-export function getInfo(str){
-  let ar = []; 
-  return new Promise((resolve, reject) => {
-    for(let i = 1; i <= 100; i++)
-    {
-      axios.get('https://swapi.dev/api/' + str + '/' + i)
-      .then(response => {
-        name = response.data.name;
-        if(str == 'people')
-          height = response.data.height;
-        else if(str == 'planets')
-          height = response.data.diameter;
-        else
-          height = response.data.length;
-        ar.push([name, height]);
-        console.log([name, height]); 
-      })
-      .catch(error => {
-        height = -1;
-      });
+export function getInfo(str, i) {
+  return axios.get('https://swapi.dev/api/' + str + '/' + i).then(
+    function (response) {
+      return response;
     }
-    setTimeout(() => resolve(ar), 5000);
-  });
+  ).catch(
+    function (error) {
+      console.log('Show error notification!')
+    }
+  )
 }
-console.log(123); 
+//console.log(123); 
